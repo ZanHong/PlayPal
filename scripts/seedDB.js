02205626880c2dcbfb6ba36e2c8b7e56113cd0ae
Zan Hong <mokzanhong92@hotmail.com>
2020-10-18 18:26:25 +1030

    included passport.js and isAuthenticated.js for Local Strategy

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const db = require("../models");
 
-// This file empties the Books collection and inserts the books below
+// This file empties the PlayPal collection and inserts the activities below
 
 mongoose.connect(
   process.env.MONGODB_URI ||
@@ -12,14 +12,14 @@ const ActivitySeed = [
   {
     title: "test 1",
     author: "test 1",
-    catergory: "1",
+    category: "1",
     description: "test 1",
     preparation: "No planning is needed. Make it fun and spontaneous for your little ones!"
   },
   {
     title: "test 2",
     author: "test 2",
-    catergory: "2",
+    category: "2",
     description: "test 2",
     preparation: "No planning is needed. Make it fun and spontaneous for your little ones!"
   }
